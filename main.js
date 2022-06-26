0120de30e6197e1eea6ba4e44292f869e2a1818d
m051978 <103782540+m051978@users.noreply.github.com>
2022-06-26 12:25:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ function count() {
     var tweet = document.getElementById("textArea").value.length;
     document.getElementById("tweetCharacters").innerHTML = tweet;
     var remaining = document.getElementById("tweetLeft").innerHTML = 280 - tweet;
+    if(remaining < 10){
+        document.getElementById('tweetLeft').style.color = "red";
+    } else{
+        document.getElementById('tweetLeft').style.color = "#000000a4";
+    }
 }
